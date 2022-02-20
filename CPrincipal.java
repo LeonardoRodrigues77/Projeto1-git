b7b9e28a8759a5ee0772f0531c5f2d78fe59b8d0
Leonardo Rodrigues dos Santos <rodriguessan77@gmail.com>
2022-02-19 21:03:09 -0300

    commit inicial

---
+++ CPrincipal.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class CPrincipal {
+
+     public static void main (String args[]){
+
+            JFrame janela = new JFrame(); // Criação da janela
+
+            janela.setSize(500,300); // tamanho da janela
+
+            janela.setTitle("Calculadora");
+
+            janela.setVisible(true); 
+
+   }
+
+}
